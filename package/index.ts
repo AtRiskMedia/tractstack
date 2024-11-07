bf6e786223ede9bc40d239f84dcf286174b5afc0
Adon <a@atriskmedia.com>
2024-11-07 15:07:53 -0500

    passing dynamic codehook

--- package/index.ts
+++ package/index.ts
@@ -3,26 +3,12 @@ import { join, dirname } from 'path';
 import defineTheme from "astro-theme-provider";
 import { z } from "astro/zod";
 
-// Get the directory path of the current module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Define the schema for the theme configuration
 const schema = z.object({
   title: z.string(),
   description: z.string().optional(),
 });
 
-// Export the theme definition
 export default defineTheme({
   name: "tractstack",
   schema,
-  setup: ({ addPage }) => {
-    const pagePath = join(__dirname, "src/pages/index.astro");
-    return {
-      components: {
-        Layout: join(__dirname, "src/layouts/Layout.astro"),
-      }
-    };
-  }
 });
--- playground/astro.config.mjs
+++ playground/astro.config.mjs
@@ -4,15 +4,17 @@ import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [TractStackTheme({
     config: {
       title: "Tract Stack",
-      description: "no-code website builder and content marketing platform"
+      description: "no-code website builder and content marketing platform",
     },
-    pages: {},
-    overrides: {}
+    overrides: {
+      components: {
+        CodeHook: './src/custom/CodeHook.astro',
+      }
+    }
   }), tailwind(), react()],
   output: "server",
   adapter: node({
